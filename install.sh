62ad14af518ea668af0e4cb7532945628c29917f
Akimon658 <akimo_greninja@yahoo.co.jp>
2023-03-22 14:15:23 +0900

    Add installing Neovim

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -s ~/.config/.bashrc ~/.bashrc
 
 rm ~/.inputrc
 ln -s ~/.config/.inputrc ~/.inputrc
+
+curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb
