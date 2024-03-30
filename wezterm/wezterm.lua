e7fb1eaee82641c14ae6220ae9bddb2bf409b11a
Takumi Akimoto <akimo_greninja@yahoo.co.jp>
2024-03-31 08:15:34 +0900

    Disable IME when WezTerm is focused

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,4 +13,12 @@ if string.find(wezterm.target_triple, "windows") then
   config.default_prog = { "pwsh", "-NoLogo" }
 end
 
+if string.find(wezterm.target_triple, "apple") then
+  wezterm.on("window-focus-changed", function(window, _)
+    if window:is_focused() then
+      os.execute "osascript -e 'tell application \"System Events\" to key code 102'"
+    end
+  end)
+end
+
 return config
