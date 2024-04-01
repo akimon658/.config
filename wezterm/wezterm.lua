71c54553621582a9556d39f105dc72aec699732b
Takumi Akimoto <akimo_greninja@yahoo.co.jp>
2024-04-01 15:42:06 +0900

    Maximize WezTerm window on startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,6 +13,11 @@ if string.find(wezterm.target_triple, "windows") then
   config.default_prog = { "pwsh", "-NoLogo" }
 end
 
+wezterm.on("gui-startup", function()
+  local _, _, window = wezterm.mux.spawn_window {}
+  window:gui_window():maximize()
+end)
+
 if string.find(wezterm.target_triple, "apple") then
   wezterm.on("window-focus-changed", function(window, _)
     if window:is_focused() then
