11b4853b11807017c2b8b38393fea601b8ca3038
Takumi Akimoto <akimoto.t.aa@m.titech.ac.jp>
2024-06-22 23:47:56 +0900

    Use `apt-get` instead of `apt`

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 
 cp -aT /workspaces/.codespaces/.persistedshare/dotfiles/ ~/.config/
 
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim.appimage --output nvim.appimage
 chmod +x nvim.appimage
