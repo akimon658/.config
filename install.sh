644534b804a882d8931bce70ca8cb2bd5b017ba2
Takumi Akimoto <akimoto.t.aa@m.titech.ac.jp>
2024-06-23 10:28:11 +0900

    Fix Neovim installation

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ cp -aT /workspaces/.codespaces/.persistedshare/dotfiles/ ~/.config/
 sudo apt-get update
 sudo apt-get upgrade -y
 
-curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim.appimage --output nvim.appimage
-chmod +x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
+curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz | tar xz
+sudo cp -r nvim-linux64/* /usr/local/
+rm -rf nvim-linux64
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 cat << 'EOS' >> ~/.bashrc
