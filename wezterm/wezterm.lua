15df29debb6691aabaa9b71b31a0325739393c52
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-26 13:46:32 +0900

    Show cwd with process icon in tab, close #283

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,6 +14,29 @@ if string.find(wezterm.target_triple, "windows") then
   config.default_prog = { "pwsh", "-NoLogo" }
 end
 
+---@param path string
+---@return string
+local function trim_path(path)
+  local s = string.gsub(path, "(.*[/\\])(.*)", "%2")
+
+  return s
+end
+
+---@type { [string]: string }
+local process_icons = {
+  ["nvim"] = wezterm.nerdfonts.custom_neovim,
+  ["zsh"] = wezterm.nerdfonts.dev_terminal,
+}
+
+wezterm.on("format-tab-title", function(tab, _, _, _, _, _)
+  local pane = tab.active_pane
+  local process_name = trim_path(pane.foreground_process_name)
+  ---@type string
+  local cwd = pane.current_working_dir.file_path
+
+  return (process_icons[process_name] or process_name .. " on") .. " " .. trim_path(cwd)
+end)
+
 wezterm.on("gui-startup", function()
   local _, _, window = wezterm.mux.spawn_window {}
   window:gui_window():maximize()
