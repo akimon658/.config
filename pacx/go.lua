db13ce467cbccca82c496fb880066937cb36ed9d
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-26 11:18:01 +0900

    Add function `uninstall` for Go

--- pacx/go.lua
+++ pacx/go.lua
@@ -1,16 +1,18 @@
----@param pkg string
-local function install(pkg)
-  local pkgName = ""
-  if pkg ~= "." then
-    pkgName = pkg .. "@latest"
-  end
-
-  os.execute("go install -ldflags='-s -w' -trimpath " .. pkgName)
-end
-
 return {
-  install = install,
+  ---@param pkg string
+  install = function(pkg)
+    local pkgName = ""
+    if pkg ~= "." then
+      pkgName = pkg .. "@latest"
+    end
+
+    os.execute("go install -ldflags='-s -w' -trimpath " .. pkgName)
+  end,
   list = function()
     os.execute "ls $(go env GOPATH)/bin"
+  end,
+  ---@param pkg string
+  uninstall = function(pkg)
+    os.execute("rm $(go env GOPATH)/bin/" .. pkg)
   end,
 }
