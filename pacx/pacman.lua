7e1445bb7062cff82597c69fb69a50f27e695628
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-23 03:36:36 +0000

    Add pacman

---
+++ pacx/pacman.lua
@@ -0,0 +1,25 @@
+---@param pkg string
+local function info(pkg)
+  os.execute("pacman -Si " .. pkg)
+end
+
+---@param pkg string
+local function install(pkg)
+  os.execute("pacman -S " .. pkg)
+end
+
+local function list()
+  os.execute "pacman -Q"
+end
+
+---@param pkg string
+local function uninstall(pkg)
+  os.execute("pacman -Rs " .. pkg)
+end
+
+return {
+  info = info,
+  install = install,
+  list = list,
+  uninstall = uninstall,
+}
