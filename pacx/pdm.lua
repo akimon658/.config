4480f805362a053618428655355ef3d17a6667a5
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-24 12:26:05 +0900

    Pass an argument to `pdm update`

--- pacx/pdm.lua
+++ pacx/pdm.lua
@@ -2,9 +2,12 @@ local function list()
   os.execute "pdm list --global"
 end
 
+---@param pkg string
+local function upgrade(pkg)
+  os.execute("pdm update --global " .. pkg)
+end
+
 return {
   list = list,
-  upgrade = function()
-    os.execute "pdm update --global"
-  end,
+  upgrade = upgrade,
 }
