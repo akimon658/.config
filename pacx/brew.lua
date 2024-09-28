64c1972f30c26f5faff95400a7d296b8527bc1e4
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-28 11:32:42 +0900

    Define `install` command for brew

--- pacx/brew.lua
+++ pacx/brew.lua
@@ -3,8 +3,14 @@ local function info(pkg)
   os.execute("brew info " .. pkg)
 end
 
+---@param pkg string
+local function install(pkg)
+  os.execute("brew install " .. pkg)
+end
+
 return {
   info = info,
+  install = install,
   upgrade = function()
     os.execute "brew upgrade"
   end,
