6279ec37e742ca2e530af282da0d9dc42112b872
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-10-13 12:17:32 +0900

    Define function `uninstall` for pnpm

--- pacx/pnpm.lua
+++ pacx/pnpm.lua
@@ -3,9 +3,15 @@ local function install(pkg)
   os.execute("pnpm add -g " .. pkg)
 end
 
+---@param pkg string
+local function uninstall(pkg)
+  os.execute("pnpm remove -g " .. pkg)
+end
+
 return {
   install = install,
   list = function()
     os.execute "pnpm list -g"
   end,
+  uninstall = uninstall,
 }
