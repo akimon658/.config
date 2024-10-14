aa4772595f37a964273d83ee8ce2d2a9bbdc2203
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-10-14 14:24:28 +0900

    Use JSON to get pnpm packages (close #285)

--- pacx/node.lua
+++ pacx/node.lua
@@ -3,33 +3,30 @@ local function install()
   os.execute "pnpm env use --global lts"
 end
 
+local function upgrade()
+  ---@type string
+  local current_version = io.popen "pnpm env list --global":read()
+
+  os.execute("pnpm env remove --global " .. current_version)
+  install()
+
+  package.path = package.path .. ";" .. os.getenv "XDG_CONFIG_HOME" .. "/pacx/json/?.lua"
+  local json = require "json"
+
+  ---@type string
+  local pnpm_list = io.popen "pnpm list --global --json":read "*a"
+  local obj = json.decode(pnpm_list)
+
+  ---@type string[]
+  local packages = {}
+  for k, _ in pairs(obj[1].dependencies) do
+    table.insert(packages, k)
+  end
+
+  os.execute("pnpm add --global " .. table.concat(packages, " "))
+end
+
 return {
   install = install,
-  upgrade = function()
-    ---@type string
-    local current_version = io.popen "pnpm env list --global":read()
-
-    os.execute("pnpm env remove --global " .. current_version)
-    install()
-
-    local pnpm_list = io.popen "pnpm list --global"
-    if not pnpm_list then
-      error "failed to execute pnpm list --global"
-    end
-
-    local in_dependencies = false
-    ---@type string[]
-    local packages = {}
-    for line in pnpm_list:lines() do
-      if in_dependencies then
-        table.insert(packages, line:match "%S+")
-      end
-
-      if line == "dependencies:" then
-        in_dependencies = true
-      end
-    end
-
-    os.execute("pnpm add --global " .. table.concat(packages, " "))
-  end,
+  upgrade = upgrade,
 }
