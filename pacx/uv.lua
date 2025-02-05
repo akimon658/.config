b760f9fec9629aea491cf2b596fc43c4db0d29e3
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-02-05 09:25:37 +0900

    Migrate to uv from PDM

--- pacx/uv.lua
+++ pacx/uv.lua
@@ -1,15 +1,15 @@
 ---@param pkg string
 local function install(pkg)
-  os.execute("pdm add --global " .. pkg)
+  os.execute("uv tool install " .. pkg)
 end
 
 local function list()
-  os.execute "pdm list --global"
+  os.execute "uv tool list"
 end
 
 ---@param pkg string
 local function upgrade(pkg)
-  os.execute("pdm update --global " .. pkg)
+  os.execute("uv tool upgrade " .. pkg)
 end
 
 return {
