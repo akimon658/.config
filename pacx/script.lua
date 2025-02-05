ceb4fe615a567cf04dabd9c4f485889beb6d1332
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-02-05 09:14:17 +0900

    Add gocryptfs installation

--- pacx/script.lua
+++ pacx/script.lua
@@ -4,11 +4,24 @@ local function install(pkg)
     error "package not specified"
   end
 
+  if pkg == "gocryptfs" then
+    local tmpdir = os.getenv "TMPDIR" .. "gocryptfs"
+
+    os.execute(string.format(
+      "mkdir -p %s && cd %s && git clone https://github.com/rfjakob/gocryptfs.git && cd gocryptfs && ./build-without-openssl.bash",
+      tmpdir, tmpdir))
+
+    return
+  end
+
   if pkg == "pnpm" then
     os.execute "curl -fsSL https://get.pnpm.io/install.sh | sh -"
+
+    return
   end
 end
 
 return {
   install = install,
+  upgrade = install,
 }
