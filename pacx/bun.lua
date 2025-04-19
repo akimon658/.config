4fe66db8df8bc7724d3b920f95c7c1452f4a9d71
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-04-19 22:53:43 +0900

    Add function `uninstall` for Bun

--- pacx/bun.lua
+++ pacx/bun.lua
@@ -3,6 +3,12 @@ local function install(pkg)
   os.execute("bun add -g " .. pkg)
 end
 
+---@param pkg string
+local function uninstall(pkg)
+  os.execute("bun remove -g " .. pkg)
+end
+
 return {
   install = install,
+  uninstall = uninstall,
 }
